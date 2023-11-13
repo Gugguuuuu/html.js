7cec633b2b4ee8b0d70e89c6342edd5a25cbdb6c
Gugguuuuu <148577471+Gugguuuuu@users.noreply.github.com>
2023-11-13 14:16:06 -0300

    Add files via upload

---
+++ html.js/main.js
@@ -0,0 +1,20 @@
+// let nomes = [ 5+3];
+// console.log(nomes)
+
+// let pessoa = {
+//     nome:"Gustavo",
+//     idade:11,
+//     notas:[10,9,8]
+// }
+// console.log(pessoa)
+
+// let objeto={}
+// objeto.nome2 = "Gustavo";
+// console.log(objeto);
+
+function click(){
+   window.location("./botao/botao.html");
+}
+
+
+
